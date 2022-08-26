8ebe2a44475489462cb07074c04d4852304bb320
EnnachatRedwan <100237071+EnnachatRedwan@users.noreply.github.com>
2022-08-26 02:13:06 +0100

    k

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -8,17 +8,13 @@ const projects_get = (req, res) => {
       res.status(403).send();
       throw err;
     }
-    con.query(
-      "call getProjects(?)",
-      user.username,
-      (err, result) => {
-        if (err) {
-          res.status(500).send();
-          throw err;
-        }
-        res.send(result[0]);
+    con.query("call getProjects(?)", user.username, (err, result) => {
+      if (err) {
+        res.status(500).send();
+        throw err;
       }
-    );
+      res.send(result[0]);
+    });
   });
 };
 
@@ -70,7 +66,8 @@ const start_project_post = (req, res) => {
   const token = req.params["token"];
   const project = req.body;
   const today = new Date();
-  const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  const date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
   jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
     if (err) {
       res.status(500).send();
@@ -78,8 +75,52 @@ const start_project_post = (req, res) => {
     }
     con.query(
       "update project set isStarted=true,startingDate=? where id_prj=? and leader=?",
-      [date,project.id, user.username],
+      [date, project.id, user.username],
       (err, result) => {
+        if (err) {
+          res.status(500).send();
+          throw err;
+        }
+        res.send();
+      }
+    );
+  });
+};
+
+const project_archive_post = (req, res) => {
+  const token = req.params["token"];
+  const project = req.body;
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) {
+      res.status(500).send();
+      throw err;
+    }
+    con.query(
+      "call archiveProject(?,?)",
+      [user.username, project.id],
+      (err) => {
+        if (err) {
+          res.status(500).send();
+          throw err;
+        }
+        res.send();
+      }
+    );
+  });
+};
+
+const project_unarchive_post = (req, res) => {
+  const token = req.params["token"];
+  const project = req.body;
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) {
+      res.status(500).send();
+      throw err;
+    }
+    con.query(
+      "delete from projectArchive where username=? and id_prj=?",
+      [user.username, project.id],
+      (err) => {
         if (err) {
           res.status(500).send();
           throw err;
@@ -95,4 +136,6 @@ module.exports = {
   project_post,
   project_delete,
   start_project_post,
+  project_archive_post,
+  project_unarchive_post,
 };
--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -1,14 +1,18 @@
-const express=require('express');
-const projectsController=require('../controllers/projectsController');
+const express = require("express");
+const projectsController = require("../controllers/projectsController");
 
-const router=express.Router();
+const router = express.Router();
 
-router.post('/start-project/:token',projectsController.start_project_post);
+router.post("/archive/:token", projectsController.project_archive_post);
 
-router.get('/:token',projectsController.projects_get);
+router.post("/unarchive/:token", projectsController.project_unarchive_post);
 
-router.post('/:token',projectsController.project_post);
+router.post("/start-project/:token", projectsController.start_project_post);
 
-router.delete('/:token',projectsController.project_delete);
+router.get("/:token", projectsController.projects_get);
 
-module.exports=router;
+router.post("/:token", projectsController.project_post);
+
+router.delete("/:token", projectsController.project_delete);
+
+module.exports = router;
