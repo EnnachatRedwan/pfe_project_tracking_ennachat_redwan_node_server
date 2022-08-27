0759b902e724180261a9b08957091e078289b0ff
EnnachatRedwan <100237071+EnnachatRedwan@users.noreply.github.com>
2022-08-27 23:33:03 +0100

    s

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -100,17 +100,13 @@ const task_archive_post = (req, res) => {
       res.status(500).send();
       throw err;
     }
-    con.query(
-      "call archiveTask(?,?)",
-      [user.username, project.id],
-      (err) => {
-        if (err) {
-          res.status(500).send();
-          throw err;
-        }
-        res.send();
+    con.query("call archiveTask(?,?)", [user.username, project.id], (err) => {
+      if (err) {
+        res.status(500).send();
+        throw err;
       }
-    );
+      res.send();
+    });
   });
 };
 
@@ -136,6 +132,94 @@ const task_unarchive_post = (req, res) => {
   });
 };
 
+const archived_tasks_get = (req, res) => {
+  const token = req.params["token"];
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) {
+      res.status(500).send();
+      throw err;
+    }
+    con.query(
+      "select t.*,p.title from task t,project p where t.id_task in (select id_task from taskArchive where username=?) and t.project_id = p.id_prj;",
+      [user.username],
+      (err) => {
+        if (err) {
+          res.status(500).send();
+          throw err;
+        }
+        console.log(result);
+        res.send();
+      }
+    );
+  });
+};
+
+const affect_task_post = (req, res) => {
+  const token = req.params["token"];
+  const data = req.body;
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) {
+      res.status(500).send();
+      throw err;
+    }
+    con.query(
+      "call affectTaskToEmployee(?,?,?);",
+      [user.username, data.taskId, data.employeeUsername],
+      (err) => {
+        if (err) {
+          res.status(500).send();
+          throw err;
+        }
+        res.send();
+      }
+    );
+  });
+};
+
+const unaffect_task_post = (req, res) => {
+  const token = req.params["token"];
+  const data = req.body;
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) {
+      res.status(500).send();
+      throw err;
+    }
+    con.query(
+      "call unaffectTaskToEmployee(?,?,?);",
+      [user.username, data.taskId, data.employeeUsername],
+      (err) => {
+        if (err) {
+          res.status(500).send();
+          throw err;
+        }
+        res.send();
+      }
+    );
+  });
+};
+
+const task_employee_get = (req, res) => {
+  
+  const token = req.params["token"];
+  const taskId = req.params["taskId"];
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) {
+      res.status(500).send();
+      throw err;
+    }
+    con.query(
+      "select username from employee where username in (select username from empTask where id_task=?)",
+      [taskId],
+      (err,result) => {
+        if (err) {
+          res.status(500).send();
+          throw err;
+        }
+        res.send(result);
+      }
+    );
+  });
+};
 
 module.exports = {
   tasks_get,
@@ -144,4 +228,8 @@ module.exports = {
   task_post,
   task_archive_post,
   task_unarchive_post,
+  archived_tasks_get,
+  affect_task_post,
+  unaffect_task_post,
+  task_employee_get
 };
--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -3,6 +3,14 @@ const tasksRouter=require('../controllers/tasksController');
 
 const router=express.Router();
 
+router.get('/employees/:token/:taskId',tasksRouter.task_employee_get);
+
+router.post('/affect/:token',tasksRouter.affect_task_post);
+
+router.post('/unaffect/:token',tasksRouter.unaffect_task_post);
+
+router.get('/archived/:token',tasksRouter.archived_tasks_get);
+
 router.post('/unarchive/:token',tasksRouter.task_unarchive_post);
 
 router.post('/archive/:token',tasksRouter.task_archive_post);
